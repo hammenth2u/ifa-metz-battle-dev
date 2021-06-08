52eb28fa9a08163f9e0169495dbbd98a55cd9921
Baptiste <dornier.baptiste@gmail.com>
2021-06-08 10:02:53 +0200

    modif header et footer

--- app/views/footer.tpl.php
+++ app/views/footer.tpl.php
@@ -1,35 +1,35 @@
-<footer class="text-center text-white bg-color-blue">
+<footer class="booking text-center text-white">
     <!-- Grid container -->
-    <div class="container p-4 pb-0">
+    <div class="p-3" style="background-color: rgba(0, 0, 0, 0.35); margin: 0;">
       <!-- Section: Social media -->
       <section class="mb-4">
         <!-- Facebook -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
           ><i class="bi bi-facebook"></i
         ></a>
 
         <!-- Twitter -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
           ><i class="bi bi-twitter"></i
         ></a>
 
         <!-- Google -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
           ><i class="bi bi-google"></i
         ></a>
 
         <!-- Instagram -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
           ><i class="bi bi-instagram"></i
         ></a>
 
         <!-- Linkedin -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
           ><i class="bi bi-linkedin"></i
         ></a>
 
         <!-- Github -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
           ><i class="bi bi-github"></i
         ></a>
       </section>
@@ -38,19 +38,13 @@
     <!-- Grid container -->
 
     <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2021 Copyright - This is Fine Inc.
+    <div class="text-center p-3">
+      © 2021 Copyright
     </div>
     <!-- Copyright -->
   </footer>
 
-  
-  <!-- JavaScript Libraries -->
-  <script src="<?=$_SERVER['BASE_URI']?>/assets/lib/jquery/jquery.min.js"></script>
-  <script src="<?=$_SERVER['BASE_URI']?>/assets/lib/bootstrap/js/bootstrap.min.js"></script>
-  <script src="<?=$_SERVER['BASE_URI']?>/assets/lib/php-mail-form/validate.js"></script>
-  <script src="<?=$_SERVER['BASE_URI']?>/assets/lib/countdown/jquery.plugin.min.js"></script>
-  <script src="<?=$_SERVER['BASE_URI']?>/assets/lib/countdown/jquery.countdown.min.js"></script>
+  <!-- JS -->
   <script src="<?=$_SERVER['BASE_URI']?>/assets/js/app.js"></script>
 </body>
 
