3ac558c1c85f2e74bf5f9e71cdcafe35e91920bc
lauhus <laurine.hussong@hotmail.fr>
2021-06-09 11:12:02 +0200

    Update game

--- app/views/game.tpl.php
+++ app/views/game.tpl.php
@@ -1,13 +1,17 @@
+
+
 <div id='app'>
-    <div class="bkgc"></div>
-    <div class="partie"> Nombre de partie joué :  {{ nb_partie }} </div> 
-    <div class="win"> Nombre de partie gagné : {{ nb_win }} </div>
-    <div class="fleche">
-        <button type="button" v-on:click="choiceDirection('left')"> Gauche </button>
-        <button type="button" v-on:click="choiceDirection('center')"> Centre </button>
-        <button type="button" v-on:click="choiceDirection('rigth')"> Droite </button>
-    </div>
-    <div class="container_goal">
-        <div class="goal"></div>
+    <div class="bkgc">
+        <div class="partie"> Nombre de parties jouées :  {{ nb_partie }} </div>
+        <div class="win"> Nombre de parties gagnées : {{ nb_win }} </div>
+        <div class="fleche">
+            <button type="button" class="btn btn-success btn-lg" v-on:click="choiceDirection('left')"> &larr; </button>
+            <button type="button" class="btn btn-success btn-lg" v-on:click="choiceDirection('center')"> &uarr; </button>
+            <button type="button" class="btn btn-success btn-lg" v-on:click="choiceDirection('rigth')"> &rarr; </button>
+        </div>
+
+        <div class="container_goal">
+            <div class="goal"></div>
+        </div>
     </div>
 </div>
