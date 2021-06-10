e9d0b4e5e52c3104da512c998d4f37d723d48215
lauhus <laurine.hussong@hotmail.fr>
2021-06-10 10:21:44 +0200

    Update game, changement avatar + fluidité

--- app/views/game.tpl.php
+++ app/views/game.tpl.php
@@ -3,17 +3,17 @@
 
 <div id='app'>
     <div v-if="game === false && restart === false" class="button_start">
-        <button class='start btn btn-primary ' v-on:click="start(true)"> START </button>
+        <button class='start btn btn-primary btn-large-custom ' v-on:click="start(true)"> START </button>
     </div>
     <div v-if="game === false && restart === true" class="button_start">
-        <button class='start btn btn-primary ' v-on:click="restart_function(true)"> Re-essayer </button>
+        <button class='start btn btn-primary btn-large-custom ' v-on:click="restart_function(true)"> REESSAYER </button>
     </div>
     <div v-if='game'>
         <div class="bkgc">
             <div class="partie"> Nombre de parties jouées :  {{ nb_partie }} </div>
             <div class="win"> Nombre de parties gagnées : {{ nb_win }} </div>
             <div v-if="win" class="result">
-                <img src="<?=$url?>/assets/img/win.png" alt="win image" />
+                <img src="<?=$url?>/assets/img/win.png" class="win_img" alt="win image" />
             </div>
             <div v-if="lost" class="result">
                 <img src="<?=$url?>/assets/img/lost.png" alt="lost image" />
@@ -21,7 +21,7 @@
             <div class="ball_container" v-if="shoot">
                 <img class="ball" src="<?=$url?>/assets/img/ball.png" alt="ballon" />
             </div>
-            <div class="fleche">
+            <div class="fleche" v-if="fleche">
                 <img class="fleche_item" src="<?=$url?>/assets/img/left.png" alt='fleche gauche' v-on:click="choiceDirection('left')" />   
                 <img class="fleche_item" src="<?=$url?>/assets/img/center.png" alt='fleche central' v-on:click="choiceDirection('center')" />   
                 <img class="fleche_item" src="<?=$url?>/assets/img/right.png" alt='fleche droite' v-on:click="choiceDirection('rigth')" />
@@ -36,6 +36,14 @@
                     <img class="goal_win" v-if="goal_win" src="<?=$url?>/assets/img/goal_win.png"/>
             </div>
         </div>
+    </div>
+    <div class="lien" v-if="link">
+        <a href='https://fr.pngtree.com/so/posture-de-rebond'>posture de rebond png de fr.pngtree.com</a>
+        <a href='https://fr.pngtree.com/so/bondir-la-balle'>bondir la balle png de fr.pngtree.com</a>
+        <a href='https://fr.pngtree.com/so/bondir-la-balle'>bondir la balle png de fr.pngtree.com</a>
+        <a href='https://fr.pngtree.com/so/posture-de-rebond'>posture de rebond png de fr.pngtree.com</a>
+        <a href='https://fr.pngtree.com/so/posture-de-rebond'>posture de rebond png de fr.pngtree.com</a>
+        <a href='https://fr.pngtree.com/so/clipart-de-football'>clipart de football png de fr.pngtree.com</a>
     </div>
 </div>
 <script src="<?=$url?>/assets/js/game.js"></script>
